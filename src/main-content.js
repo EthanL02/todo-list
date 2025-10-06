5fbb1eb800db32b7881e3489bc96e23dfdc24e97
Ethan Lopez <102267044+EthanL02@users.noreply.github.com>
2025-10-06 15:46:40 -0400

    Load main content if project has no todos
    Display 'empty project' if no todos detected

--- src/main-content.js
+++ src/main-content.js
@@ -1,5 +1,13 @@
+const mainContent = document.getElementById("main-content");
+const emptyLabel = mainContent.querySelector("h2");
+
 function update(todos) {
     console.log(todos);
+    if (todos.length !== 0) {
+        emptyLabel.style.display = "none";
+    } else {
+        emptyLabel.style.display = "block";
+    }
 }
 
 export default { update };
