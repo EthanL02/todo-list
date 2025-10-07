96de07bcf80308746e8964d005a4e31ebfd787b4
Ethan Lopez <102267044+EthanL02@users.noreply.github.com>
2025-10-07 19:33:16 -0400

    Move project list to index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
 import "./styles.css";
 import todo from "./todo.js";
-import projects from "./projects.js";
+import project from "./project.js";
 import displayController from "./display-controller.js";
 
 document.documentElement.setAttribute("data-theme", "light");
 
-projects.addProject("My first project");
-projects.addProject("My second project");
-projects.addProject("My third project");
+const projects = [];
 
-let curProject = projects.find(0);
+function addProject(newProject) {
+    projects.push(project.createNewProject(newProject));
+}
+
+addProject("My first project");
+addProject("My second project");
+addProject("My third project");
+
+let curProject = projects[0];
 
 const todo1 = todo.createNewTodo("Item # 1", "Description for first item", "4/16/25", 5);
 const todo2 = todo.createNewTodo("Item # 2", "Description for second item", "5/07/26", 5);
 curProject.addTodo(todo1);
 curProject.addTodo(todo2);
 
-displayController.update(projects.getAll(), curProject);
+displayController.update(projects, curProject);
