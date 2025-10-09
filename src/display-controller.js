a94f1601b9c3fa28005580f9f25efa34d01e8cff
Ethan Lopez <102267044+EthanL02@users.noreply.github.com>
2025-10-08 20:54:22 -0400

    Add update to sidebar buttons
    Extract update main-content for main-content only updates

--- src/display-controller.js
+++ src/display-controller.js
@@ -2,13 +2,15 @@ const sideBar = document.getElementById("side-bar");
 const newProjectButton = document.getElementById("new-project");
 const mainContent = document.getElementById("main-content");
 
+let curProject;
+
 function createProject(project) {
     const button = document.createElement("button");
     button.classList.add("project");
     button.textContent = project.name;
 
     button.addEventListener("click", () => {
-        update(project.todos);
+        updateMainContent(project);
     });
     sideBar.prepend(button);
 }
@@ -19,27 +21,36 @@ function createTodo(todo) {
     mainContent.append(background);
 }
 
-function clear() {
+function clearSidebar() {
     while (sideBar.firstChild !== newProjectButton) {
         sideBar.removeChild(sideBar.firstChild);
     }
+}
 
+function clearMainContent() {
     while (mainContent.firstChild) {
         mainContent.removeChild(mainContent.firstChild);
     }
 }
 
-function update(projects, curProject) {
-    clear();
+function update(projects, newCurProject) {
+    curProject = newCurProject !== undefined ? newCurProject : curProject;
 
     //Load sidebar with each project
+    clearSidebar();
+
     projects.forEach(project => {
         createProject(project);
     });
 
     //Load main-content with curProject's todos
-    curProject.todos.forEach(todo => {
-        createTodo(todo);
+    updateMainContent(curProject);
+}
+
+function updateMainContent(project) {
+    clearMainContent();
+    project.todos.forEach(todo => {
+        createTodo();
     });
 }
 
